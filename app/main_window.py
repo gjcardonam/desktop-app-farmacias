300455634f0bd607531b7dd0ab832235a567edb1
Geofon7 <geofon7@gmail.com>
2025-07-07 17:08:20 -0400

    se agregan alguno from

--- app/main_window.py
+++ app/main_window.py
@@ -14,12 +14,13 @@ from app.usuarios.gui.usuario_form import mostrar_formulario_usuario
 from app.clientes.gui.cliente_form import mostrar_formulario_cliente
 from app.proveedores.gui.proveedor_form import mostrar_formulario_proveedor
 from app.proveedores.gui.listado_proveedores import mostrar_listado_proveedores
-from utils.ui_utils import centrar_ventana, activar_enter_en_todos_los_botones, permitir_tab_en_botones
+from utils.ui_utils import centrar_ventana, activar_enter_en_todos_los_botones, permitir_tab_en_botones, activar_enter_en_treeviews
 
 def start_main_window():
     root = tk.Tk()
     permitir_tab_en_botones(root)
     activar_enter_en_todos_los_botones(root)
+    activar_enter_en_treeviews(root, doble_clic=True)
     root.title("Farmacontable")
     root.geometry("800x600")
     centrar_ventana(root, 800, 600)
